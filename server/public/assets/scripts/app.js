0f241d813338a50cf09f0fd4b78136eb54041e36
mmpyhmm <choipui@gmail.com>
2015-11-09 16:14:35 -0600

    Initial commit

---
+++ server/public/assets/scripts/app.js
@@ -0,0 +1,75 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('WelcomeController', ['$scope', '$http', function($scope, $http){
+    $scope.note = {};
+    $scope.nameArray=[];
+
+    //POST
+    $scope.clickButton = function(note){
+        $http.post('/post/data', note).then(function(response){
+           $scope.getPeople();
+        });
+    };
+
+    //GET
+    $scope.getPeople = function(){
+        $http.get('/post/data').then(function(response){
+            $scope.nameArray = response.data;
+        });
+    };
+
+    $scope.getPeople();
+
+}]);
+
+
+//$(document).ready(function(){
+//    getData();
+//    $("#messageForm").submit(function(event){
+//        event.preventDefault();
+//        var values = {};
+//
+//        $.each($(this).serializeArray(), function(i, field){
+//            values[field.name] = field.value;
+//        });
+//
+//        $("#messageForm").find("input[type=text]").val("");
+//        $("#messageForm").find("textarea[type=text]").val("");
+//
+//        postData(values);
+//    });
+//});
+//
+//function getData(){
+//    $.ajax({
+//        type: "GET",
+//        url: "/post/data",
+//        success: function(data){
+//            updateDOM(data);
+//        }
+//    });
+//}
+//
+//function postData(values){
+//    $.ajax({
+//        type: "POST",
+//        url: "/post/data",
+//        data: values,
+//        success: function(){
+//            getData();
+//        }
+//    });
+//}
+//
+//function updateDOM(data){
+//    $("#messageContainer").empty();
+//
+//    for(var i = 0; i < data.length; i++){
+//        var el = "<div class='messageBlock'>" +
+//            "<p class='name'>" + data[i].name + " said: </p>" +
+//            "<p class='message'>" + data[i].message + "</p></div>";
+//
+//        $("#messageContainer").append(el);
+//    }
+//
+//}
